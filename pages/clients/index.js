20767ad4e3c8fdefaafdad5375189ed23128402c
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-07-09 00:49:11 +0200

    Navigating to dynamic routes

--- pages/clients/index.js
+++ pages/clients/index.js
@@ -1,7 +1,20 @@
+import Link from "next/link";
+
 const ClientsPage = () => {
+  const clients = [
+    { id: "adri", name: "Adrián" },
+    { id: "alex", name: "Alejandro" },
+  ];
   return (
     <div>
       <h1>The Clients Page</h1>
+      <ul>
+        {clients.map((client) => (
+          <li key={client.id}>
+            <Link href={`/clients/${client.id}`}>{client.name}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import Head from "next/head";
 import Image from "next/image";
+import Link from "next/link";
 
 const Home = () => {
   return (
     <div>
       <h1>The Home Page</h1>
+      <ul>
+        <li>
+          <Link href="/portfolio">Portfolio</Link>
+        </li>
+        <li>
+          <Link href="/clients">Clients</Link>
+        </li>
+      </ul>
     </div>
   );
 };
