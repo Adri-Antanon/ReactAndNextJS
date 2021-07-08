8d07fe0e8f45b80b4788fc1e58bb4ec2955ce364
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-07-09 01:06:46 +0200

    Add custom 404

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -1,7 +1,16 @@
+import { useRouter } from "next/router";
+
 const ClientProjectPage = () => {
+  const router = useRouter();
+
+  const loadProjectHandler = () => {
+    // Load data
+    router.push("/clients/adri/A");
+  };
   return (
     <div>
       <h1>The projects of given client</h1>
+      <button onClick={loadProjectHandler}>Load project A</button>
     </div>
   );
 };
