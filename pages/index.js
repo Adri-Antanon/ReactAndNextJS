067b7f3c9c5682b4416f0f18e502271cb8f45dc2
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-07-14 00:02:15 +0200

    running server side code & using the filesystem

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import fs from "fs/promises";
+import path from "path";
 
 export default function Home(props) {
   const { products } = props;
@@ -14,9 +13,13 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
+  const filepath = path.join(process.cwd(), "data", "dummy-backend.json");
+  const jsonData = await fs.readFile(filepath);
+  const data = JSON.parse(jsonData);
+
   return {
     props: {
-      products: [{ id: "p1", title: "Product 1" }],
+      products: data.products,
     },
   };
 }
