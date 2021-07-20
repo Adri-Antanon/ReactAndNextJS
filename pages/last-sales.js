9bed3d76482f1f7b1f7e7c99b42bda37c94a78a7
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-07-20 12:25:27 +0200

    feat: using the useSWR nextHook

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -1,35 +1,55 @@
 import { useEffect, useState } from "react";
+import useSWR from "swr";
 
 const BASE_URL = "https://nextjsexample-e9557-default-rtdb.firebaseio.com/";
 
 function LastSalesPage() {
   const [sales, setSales] = useState();
   const [isLoading, setIsLoading] = useState(false);
-  useEffect(() => {
-    setIsLoading(true);
-    fetch(BASE_URL + "/sales.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const transformedSales = [];
-        for (const key in data) {
-          transformedSales.push({
-            id: key,
-            username: data[key].username,
-            volume: data[key].volume,
-          });
-        }
-        setSales(transformedSales);
-        setIsLoading(false);
-      });
-  }, []);
 
-  if (isLoading) {
+  // Esta hook creado por Vercel sustituye al custom hook que he creado abajo
+  const { data, error } = useSWR(BASE_URL + "/sales.json");
+  useEffect(() => {
+    if (data) {
+      const transformedSales = [];
+      for (const key in data) {
+        transformedSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+      setSales(transformedSales);
+    }
+  }, [data]);
+
+  // ESTE CUSTOM HOOK
+  // useEffect(() => {
+  //   setIsLoading(true);
+  //   fetch(BASE_URL + "/sales.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const transformedSales = [];
+  //       for (const key in data) {
+  //         transformedSales.push({
+  //           id: key,
+  //           username: data[key].username,
+  //           volume: data[key].volume,
+  //         });
+  //       }
+  //       setSales(transformedSales);
+  //       setIsLoading(false);
+  //     });
+  // }, []);
+
+  if (error) {
+    return <p>Failed to load...</p>;
+  }
+
+  if (!data || !sales) {
     return <p>Loading...</p>;
   }
 
-  if (!sales) {
-    return <p>No data yet</p>;
-  }
   return (
     <ul>
       {sales.map((sale) => (
