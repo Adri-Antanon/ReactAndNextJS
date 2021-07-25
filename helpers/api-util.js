97ff46f2d0bd44b610c46a637bdeb06c7edec4ef
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-07-25 17:41:24 +0200

    refactor: :zap:

---
+++ helpers/api-util.js
@@ -0,0 +1,23 @@
+const BASE_URL = "https://nextjsexample-e9557-default-rtdb.firebaseio.com/";
+
+export async function getAllEvents() {
+  const response = await fetch(BASE_URL + "events.json");
+  const data = await response.json();
+
+  const events = [];
+
+  for (const key in data) {
+    events.push({
+      id: key,
+      ...data[key],
+    });
+  }
+
+  return events;
+}
+
+export async function getFeaturedEvents() {
+  const allEvents = await getAllEvents();
+
+  return allEvents.filter((event) => event.isFeatured);
+}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,20 @@
 import EventList from "../components/events/eventList";
 
-import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 
-const Home = () => {
-  const featureEvents = getFeaturedEvents();
-  return <EventList items={featureEvents} />;
+const HomePage = (props) => {
+  const { events } = props;
+
+  return <EventList items={events} />;
 };
 
-export default Home;
+export async function getStaticProps() {
+  const featuredEvents = await getFeaturedEvents();
+
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
+}
+export default HomePage;
