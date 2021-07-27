13466ee1abff4c5128899143f7b639459298db5d
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-07-27 11:59:30 +0200

    feat: configure the head content

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import EventList from "../components/events/eventList";
 
 import { getFeaturedEvents } from "../helpers/api-util";
@@ -5,7 +6,18 @@ import { getFeaturedEvents } from "../helpers/api-util";
 const HomePage = (props) => {
   const { events } = props;
 
-  return <EventList items={events} />;
+  return (
+    <div>
+      <Head>
+        <title>NextJS Events</title>
+        <meta
+          name="description"
+          content="Find a lot of great events that allow you to evolve..."
+        />
+      </Head>
+      <EventList items={events} />
+    </div>
+  );
 };
 
 export async function getStaticProps() {
