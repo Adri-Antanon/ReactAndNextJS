95c10f32197a6f51d0e925b8d85232dd88e6a30f
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-10 22:05:39 +0200

    Creating & using dynamic api routes

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,12 +1,29 @@
+import { useState } from "react";
 import { extractFeedback, buildFeedbackPath } from "../api/feedback";
 
 export default function FeedbackPage(props) {
+  const [feedbackData, setFeedbackData] = useState();
+  function loadFeedbackHandler(id) {
+    fetch(`/api/${id}`)
+      .then((response) => response.json())
+      .then((data) => {
+        setFeedbackData(data.feedback);
+      });
+  }
   return (
-    <ul>
-      {props.feedbackItems.map((item) => (
-        <li key={item.id}>{item.text}</li>
-      ))}
-    </ul>
+    <>
+      {feedbackData && <p>{feedbackData.email}</p>}
+      <ul>
+        {props.feedbackItems.map((item) => (
+          <li key={item.id}>
+            {item.text}
+            <button onClick={loadFeedbackHandler.bind(null, item.id)}>
+              Show details
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
 
