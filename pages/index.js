eb6fc3fbd9924844498965ff7369c33ce1f9cfb2
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-10 12:21:20 +0200

    feat: :zap: preparing the frontend form

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
 import Head from "next/head";
 import Image from "next/image";
 
+import { useRef } from "react";
+
 export default function Home() {
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    event.preventDefault();
+  }
   return (
     <div>
       <h1>The Home Page</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <textarea
+            name="feedback"
+            id="feedback"
+            rows="5"
+            ref={feedbackInputRef}
+          ></textarea>
+        </div>
+        <button>Send feedback</button>
+      </form>
     </div>
   );
 }
