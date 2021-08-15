06c63f3f7d8000c90acf76eb19fbdd054a123fce
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-16 00:32:09 +0200

    feat: add comments api routes

---
+++ pages/api/comments/[eventId].js
@@ -0,0 +1,38 @@
+function handler(req, res) {
+  const eventId = req.query.eventId;
+
+  if (req.method === "POST") {
+    //   add server-side validation
+    const { emailAddress, name, text } = req.body;
+
+    if (
+      !email.includes("@") ||
+      !name ||
+      name.trim() === "" ||
+      !text ||
+      text.trim() === ""
+    ) {
+      res.status(422).json({ message: "Invalid input." });
+      return;
+    }
+    const newComment = {
+      id: new Date().toISOString(),
+      email,
+      name,
+      text,
+    };
+    console.log(newComment);
+    res.status(201).json({ message: "Added comment", comment: newComment });
+  }
+
+  if (req.method === "GET") {
+    const dummyList = [
+      { id: "c1", name: "Adri", text: "Hi, this is a comment" },
+      { id: "c2", name: "Andrea", text: "Hi, I want a pet so I choose Zeus!" },
+    ];
+
+    res.status(200).json({ comments: dummyList });
+  }
+}
+
+export default handler;
