52c5e2e061ac441429bcdfbcf412ed84f7e87cca
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-15 16:30:11 +0200

    feat: add a newsletter route

---
+++ pages/api/newsletter.js
@@ -0,0 +1,14 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const userEmail = req.body.email;
+    if (!userEmail || !userEmail.includes("@")) {
+      res.status(422).json({ message: "invalid email address." });
+      return;
+    }
+
+    console.log(userEmail);
+    res.status(201).json({ messagge: "Signed up!", userEmail });
+  }
+}
+
+export default handler;
