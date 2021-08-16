027deb28e63dda96bb376d45e2310771993f305d
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-17 01:56:22 +0200

    feat: creating a new react context

--- pages/_app.js
+++ pages/_app.js
@@ -2,20 +2,26 @@ import Head from "next/head";
 
 import Layout from "../components/layout/layout";
 import Notification from "../components/ui/notification";
+import { NotificationContextProvider } from "../store/notification-context";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Head>
-        <title>Next Events</title>
-        <meta name="description" content="NextJS Events" />
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
-      <Component {...pageProps} />
-      <Notification title="Test" message="This is a test." status="pending" />
-    </Layout>
+    <NotificationContextProvider>
+      <Layout>
+        <Head>
+          <title>Next Events</title>
+          <meta name="description" content="NextJS Events" />
+          <meta
+            name="viewport"
+            content="initial-scale=1.0, width=device-width"
+          />
+        </Head>
+        <Component {...pageProps} />
+        <Notification title="Test" message="This is a test." status="pending" />
+      </Layout>
+    </NotificationContextProvider>
   );
 }
 
