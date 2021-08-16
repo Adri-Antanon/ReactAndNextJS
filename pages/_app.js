0ea425612db81e0b9a5b0481dfc00eed60299d00
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-17 01:46:54 +0200

    feat: :zap: starting project

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 
 import Layout from "../components/layout/layout";
+import Notification from "../components/ui/notification";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
@@ -12,6 +14,7 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
+      <Notification title="Test" message="This is a test." status="pending" />
     </Layout>
   );
 }
