ead6cab06cf0b8213203aab30f74f47466199c29
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-26 12:00:08 +0200

    feat: :zap: add layout & nav

---
+++ components/layout/main-navigation.js
@@ -0,0 +1,26 @@
+import Logo from "./logo";
+import Link from "next/link";
+
+const MainNavigation = () => {
+  return (
+    <header>
+      <Link href="/">
+        <a>
+          <Logo />
+        </a>
+      </Link>
+      <nav>
+        <ul>
+          <li>
+            <Link href="/posts">Posts</Link>
+          </li>
+          <li>
+            <Link href="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default MainNavigation;
