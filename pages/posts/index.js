baf309c9bd502bf49cea4f5f230adb6007d76dbb
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-27 15:55:01 +0200

    feat: add the all posts page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,5 +1,48 @@
-const AllPostPage = () => {
-  return <div></div>;
+import { AllPosts } from "../../components/posts";
+const DUMMY_POSTS = [
+  {
+    slug: "getting-started-with-nextjs",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a React framework for production - it makes building fullstack React Apps and sites a breeze and ships with built-in SSR",
+    date: "2021-08-26",
+  },
+  {
+    slug: "getting-started-with-nextjs2",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a React framework for production - it makes building fullstack React Apps and sites a breeze and ships with built-in SSR",
+    date: "2021-08-26",
+  },
+  {
+    slug: "getting-started-with-nextjs3",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a React framework for production - it makes building fullstack React Apps and sites a breeze and ships with built-in SSR",
+    date: "2021-08-26",
+  },
+  {
+    slug: "getting-started-with-nextjs4",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a React framework for production - it makes building fullstack React Apps and sites a breeze and ships with built-in SSR",
+    date: "2021-08-26",
+  },
+  {
+    slug: "getting-started-with-nextjs5",
+    title: "Getting Started with NextJS",
+    image: "getting-started-nextjs.png",
+    excerpt:
+      "NextJS is a React framework for production - it makes building fullstack React Apps and sites a breeze and ships with built-in SSR",
+    date: "2021-08-26",
+  },
+];
+const AllPostsPage = () => {
+  return <AllPosts posts={DUMMY_POSTS} />;
 };
 
-export default AllPostPage;
+export default AllPostsPage;
