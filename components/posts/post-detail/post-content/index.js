65abec20878a1ca2ae593fd0e2c43e5d7f0abab1
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-31 18:30:46 +0200

    feat: :art: redering images with next/image component from md

--- components/posts/post-detail/post-content/index.js
+++ components/posts/post-detail/post-content/index.js
@@ -2,23 +2,49 @@ import ReactMarkdown from "react-markdown";
 import { PostHeader } from "..";
 
 import classes from "./post-content.module.css";
+import Image from "next/image";
 
-// const DUMMY_POST = {
-//   slug: "getting-started-with-nextjs",
-//   title: "Getting Started with NextJS",
-//   image: "getting-started-nextjs.png",
-//   date: "2021-08-26",
-//   content: "# This is a first post",
-// };
-
-export const PostContent = ({ post }) => {
+export const PostContent = (props) => {
+  const { post } = props;
   const imagePath = `/images/posts/${post.slug}/${post.image}`;
+
+  const customRenderers = {
+    // img(image) {
+    //   return (
+    //     <Image
+    //       src={`/images/posts/${post.slug}/${image.src}`}
+    //       alt={image.alt}
+    //       width={600}
+    //       height={300}
+    //     />
+    //   );
+    // },
+    p(paragraph) {
+      const { node } = paragraph;
+
+      if (node.children[0].tagName === "img") {
+        const image = node.children[0];
+        return (
+          <div className={classes.image}>
+            <Image
+              src={`/images/posts/${post.slug}/${image.properties.src}`}
+              alt={image.alt}
+              width={600}
+              height={300}
+            />
+          </div>
+        );
+      }
+      return <p>{paragraph.children}</p>;
+    },
+  };
   return (
     <article className={classes.content}>
       <PostHeader title={post.title} image={imagePath} />
-      <ReactMarkdown>{post.content}</ReactMarkdown>
+      <ReactMarkdown components={customRenderers}>{post.content}</ReactMarkdown>
       {/* Esto solo funciona con la versión 6.0.0*/}
-      {/* <ReactMarkdown children={post.content} /> Esto debería de funcionar con la versión 7.0.1 pero no me va, debido a que no es estable todavía */}
+      {/* <ReactMarkdown children={post.content} />  */}
+      {/*Esto debería de funcionar con la versión 7.0.1 pero no me va, debido a que no es estable todavía */}
     </article>
   );
 };
