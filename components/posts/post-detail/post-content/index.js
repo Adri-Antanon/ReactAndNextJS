d2109b5f3157a488bbf77ec473da11df6efc81e1
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-08-31 23:03:52 +0200

    feat: :art: rendering code snippets from md

--- components/posts/post-detail/post-content/index.js
+++ components/posts/post-detail/post-content/index.js
@@ -1,8 +1,11 @@
 import ReactMarkdown from "react-markdown";
+import Image from "next/image";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+
 import { PostHeader } from "..";
 
 import classes from "./post-content.module.css";
-import Image from "next/image";
 
 export const PostContent = (props) => {
   const { post } = props;
@@ -36,6 +39,18 @@ export const PostContent = (props) => {
         );
       }
       return <p>{paragraph.children}</p>;
+    },
+
+    code(code) {
+      const { className, children } = code;
+      const language = className.split("-")[1]; // className is something like language-js => We need the "js" part here
+      return (
+        <SyntaxHighlighter
+          style={atomDark}
+          language={language}
+          children={children}
+        />
+      );
     },
   };
   return (
