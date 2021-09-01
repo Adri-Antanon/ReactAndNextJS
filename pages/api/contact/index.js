cd12e3bbed5c3d5b63252f5665921e49d85d2a78
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-09-01 11:44:33 +0200

    feat: :rocket: add nextjs config file & env variables

--- pages/api/contact/index.js
+++ pages/api/contact/index.js
@@ -23,10 +23,11 @@ async function handler(req, res) {
       message,
     };
     let client;
+
+    const connectionString = `mongodb+srv://${process.env.mongodb_username}:${process.env.mongodb_password}@${process.env.mongodb_clustername}.bykul.mongodb.net/${process.env.mongodb_database}?retryWrites=true&w=majority`;
+
     try {
-      client = await MongoClient.connect(
-        "mongodb+srv://Adri:Contra123@nextjscluster.bykul.mongodb.net/blog?retryWrites=true&w=majority"
-      );
+      client = await MongoClient.connect(connectionString);
     } catch (error) {
       res.status(500).json({ message: "Could not connect to database" });
       return;
