6e5730e5293d494f97e97c2e28e30f4b9dc28d17
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-09-01 12:03:00 +0200

    perf: :white_check_mark: checking & optimizing code

--- components/posts/post-detail/post-content/index.js
+++ components/posts/post-detail/post-content/index.js
@@ -1,7 +1,12 @@
 import ReactMarkdown from "react-markdown";
 import Image from "next/image";
-import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { atomDark } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import { PrismLight as SyntaxHighlighter } from "react-syntax-highlighter";
+import atomDark from "react-syntax-highlighter/dist/cjs/styles/prism/atom-dark";
+import js from "react-syntax-highlighter/dist/cjs/languages/prism/javascript";
+import css from "react-syntax-highlighter/dist/cjs/languages/prism/css";
+
+SyntaxHighlighter.registerLanguage("js", js);
+SyntaxHighlighter.registerLanguage("css", css);
 
 import { PostHeader } from "..";
 
