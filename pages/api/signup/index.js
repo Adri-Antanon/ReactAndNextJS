c338c8acc454fa333a6c5600ecb2a30cb2810a0e
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-09-04 23:43:34 +0200

    feat: :zap: add a user signup api route

--- pages/api/signup/index.js
+++ pages/api/signup/index.js
@@ -1,3 +1,35 @@
-function handler() {}
+import { hashPassword } from "../../../lib/auth";
+import { connectToDatabase } from "../../../lib/db";
+
+async function handler(req, res) {
+  const data = req.body;
+
+  const { email, password } = data;
+
+  if (
+    !email ||
+    !email.includes("@") ||
+    !password ||
+    password.trim().length < 7
+  ) {
+    res.status(422).json({
+      message:
+        "Invalid input - password should also be at least 7 characters long.",
+    });
+  }
+
+  const client = await connectToDatabase();
+
+  const db = client.db();
+
+  const hashedPassword = hashPassword(password);
+
+  const result = await db.collection("users").insertOne({
+    email: email,
+    password: hashedPassword,
+  });
+
+  res.status(201).json({ message: "Created user!" });
+}
 
 export default handler;
