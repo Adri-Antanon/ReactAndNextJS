28ed7a2e1727189eb408dba3ba0672e64103bf61
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-09-05 16:03:22 +0200

    feat: :ambulance: hide credentials & use env variables

---
+++ next.config.js
@@ -0,0 +1,23 @@
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {
+        mongodb_username: "Adri",
+        mongodb_password: "Contra123",
+        mongodb_clustername: "nextjscluster",
+        mongodb_database: "authentication-dev",
+      },
+    };
+  }
+
+  return {
+    env: {
+      mongodb_username: "Adri",
+      mongodb_password: "Contra123",
+      mongodb_clustername: "nextjscluster",
+      mongodb_database: "authentication",
+    },
+  };
+};
