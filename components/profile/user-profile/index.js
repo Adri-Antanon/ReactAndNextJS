0c81192dc5efe168a43ff3853c31ea3ccaab2f34
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-09-06 10:14:56 +0200

    feat: :rotating_light: add server side page guards

--- components/profile/user-profile/index.js
+++ components/profile/user-profile/index.js
@@ -1,26 +1,28 @@
-import { useRouter } from "next/router";
-import { getSession } from "next-auth/client";
-import { useEffect, useState } from "react";
+// import { useRouter } from "next/router";
+// import { useEffect, useState } from "react";
+// import { getSession } from "next-auth/client";
+
 import { ProfileForm } from "../profile-form";
 import classes from "./user-profile.module.css";
 
 export const UserProfile = () => {
-  const router = useRouter();
-  const [isLoading, setIsLoading] = useState(true);
+  // First way without getServerSideProps
+  // const router = useRouter();
+  // const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        router.push("/auth");
-      } else {
-        setIsLoading(false);
-      }
-    });
-  }, []);
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       router.push("/auth");
+  //     } else {
+  //       setIsLoading(false);
+  //     }
+  //   });
+  // }, []);
 
-  if (isLoading) {
-    return <p className={classes.profile}>Loading...</p>;
-  }
+  // if (isLoading) {
+  //   return <p className={classes.profile}>Loading...</p>;
+  // }
 
   return (
     <section className={classes.profile}>
