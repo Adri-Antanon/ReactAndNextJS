f64b05c5950be5b7cc4b216c73355dfe2ba16ee9
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-09-06 09:30:36 +0200

    feat: :sparkles: add clien-side page guards

--- components/profile/user-profile/index.js
+++ components/profile/user-profile/index.js
@@ -1,8 +1,26 @@
+import { useRouter } from "next/router";
+import { getSession } from "next-auth/client";
+import { useEffect, useState } from "react";
 import { ProfileForm } from "../profile-form";
 import classes from "./user-profile.module.css";
 
 export const UserProfile = () => {
-  // Redirect away if NOT auth
+  const router = useRouter();
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        router.push("/auth");
+      } else {
+        setIsLoading(false);
+      }
+    });
+  }, []);
+
+  if (isLoading) {
+    return <p className={classes.profile}>Loading...</p>;
+  }
 
   return (
     <section className={classes.profile}>
