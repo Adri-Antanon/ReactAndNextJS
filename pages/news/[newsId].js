65a8f1565a74dd70a0b590f75374e058fdb866b5
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-09-12 19:00:16 +0200

    feat: :zap: link between pages

--- pages/news/[newsId].js
+++ pages/news/[newsId].js
@@ -1,4 +1,10 @@
+import { useRouter } from "next/router";
+
 const DetailPage = () => {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
   return <h1>The detail page</h1>;
 };
 
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,5 +1,16 @@
+import Link from "next/link";
+
 const NewsPage = () => {
-  return <h1>The News Page</h1>;
+  return (
+    <>
+      <h1>The News Page</h1>
+      <ul>
+        <li>
+          <Link href="/news">News</Link>
+        </li>
+      </ul>
+    </>
+  );
 };
 
 export default NewsPage;
