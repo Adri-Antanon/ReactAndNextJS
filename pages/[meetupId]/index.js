054840b77d7ff6c630f182f62728c42b82343486
Adrian Antanon Orozco <adrianantanon@gmail.com>
2021-09-14 11:46:30 +0200

    feat: :zap: add getStaticPaths & dynamic pages

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,40 @@
 import { MeetupDetail } from "../../components/meetups";
 
-const MeetupDetails = () => {
+const MeetupDetails = ({ meetupData }) => {
+  const { image, title, address, description } = meetupData;
   return (
     <MeetupDetail
-      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg"
-      title="A First Meetup"
-      address="Some Street 5, Some City"
-      description="This is the first meetup"
+      image={image}
+      title={title}
+      address={address}
+      description={description}
     />
   );
 };
+
+export async function getStaticPaths() {
+  return {
+    paths: [{ params: { meetupId: "m1" } }],
+    fallback: "blocking",
+  };
+}
+
+export async function getStaticProps(context) {
+  // Fetch data for a single meetup
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg",
+        title: "A First Meetup",
+        address: "Some Street 5, Some City",
+        description: "This is the first meetup",
+        id: meetupId,
+      },
+    },
+  };
+}
 
 export default MeetupDetails;
